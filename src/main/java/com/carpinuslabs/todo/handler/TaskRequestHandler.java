fb0704503a5ed95c9468c04258a18386b6df8429
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2019-02-03 21:41:54 +0100

    First version of the model

--- src/main/java/com/carpinuslabs/todo/handler/TaskRequestHandler.java
+++ src/main/java/com/carpinuslabs/todo/handler/TaskRequestHandler.java
@@ -1,19 +1,42 @@
-package com.carpinuslabs.todo;
+package com.carpinuslabs.todo.handler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.carpinuslabs.todo.model.Task;
 
 /**
  * Entrypoints for API Gateway
  */
 public class TaskRequestHandler {
   public APIGatewayProxyResponseEvent listTasks(APIGatewayProxyRequestEvent request, Context context) {
-    Map<String, String> headers = new HashMap<String, String>();
-    headers.put("Content-Type", "text/plain");
-    return new APIGatewayProxyResponseEvent().withStatusCode(200).withHeaders(headers).withBody("Hello, world!");
+    Task task = new Task();
+    task.setTitle("Some Task");
+    task.setDescription("Lorem ipsum dolor est");
+
+    List<Task> tasks = new ArrayList<Task>();
+    tasks.add(task);
+
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      String jsonInString = mapper.writeValueAsString(task);
+      System.out.println(jsonInString);
+
+      Map<String, String> headers = new HashMap<String, String>();
+      headers.put("Content-Type", "application/json");
+      return new APIGatewayProxyResponseEvent().withStatusCode(200).withHeaders(headers).withBody(jsonInString);
+    } catch(JsonProcessingException e) {
+      e.printStackTrace();
+      return new APIGatewayProxyResponseEvent().withStatusCode(500);
+    }
   }
 }
