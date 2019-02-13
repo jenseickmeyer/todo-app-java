df7265fe5a923da99e67f7608879e02602efdbf8
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2019-02-13 06:32:22 +0100

    Reformatting

--- src/main/java/com/carpinuslabs/todo/handler/TaskRequestHandler.java
+++ src/main/java/com/carpinuslabs/todo/handler/TaskRequestHandler.java
@@ -18,44 +18,44 @@ import com.carpinuslabs.todo.model.Task;
  * Entrypoints for API Gateway
  */
 public class TaskRequestHandler {
+	
+	public APIGatewayProxyResponseEvent listTasks(APIGatewayProxyRequestEvent request, Context context) {
+		Task task = new Task();
+		task.setTitle("Some Task");
+		task.setDescription("Lorem ipsum dolor est");
 
-  public APIGatewayProxyResponseEvent listTasks(APIGatewayProxyRequestEvent request, Context context) {
-    Task task = new Task();
-    task.setTitle("Some Task");
-    task.setDescription("Lorem ipsum dolor est");
+		List<Task> tasks = new ArrayList<Task>();
+		tasks.add(task);
 
-    List<Task> tasks = new ArrayList<Task>();
-    tasks.add(task);
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			String jsonInString = mapper.writeValueAsString(tasks);
 
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      String jsonInString = mapper.writeValueAsString(tasks);
+			Map<String, String> headers = new HashMap<String, String>();
+			headers.put("Content-Type", "application/json");
+			return new APIGatewayProxyResponseEvent().withStatusCode(200).withHeaders(headers).withBody(jsonInString);
+		} catch(JsonProcessingException e) {
+			e.printStackTrace();
+			return new APIGatewayProxyResponseEvent().withStatusCode(500);
+		}
+	}
 
-      Map<String, String> headers = new HashMap<String, String>();
-      headers.put("Content-Type", "application/json");
-      return new APIGatewayProxyResponseEvent().withStatusCode(200).withHeaders(headers).withBody(jsonInString);
-    } catch(JsonProcessingException e) {
-      e.printStackTrace();
-      return new APIGatewayProxyResponseEvent().withStatusCode(500);
-    }
-  }
+	public APIGatewayProxyResponseEvent getTask(APIGatewayProxyRequestEvent request, Context context) {
+		Task task = new Task();
+		task.setTitle("Some Task");
+		task.setDescription("Lorem ipsum dolor est");
 
-  public APIGatewayProxyResponseEvent getTask(APIGatewayProxyRequestEvent request, Context context) {
-    Task task = new Task();
-    task.setTitle("Some Task");
-    task.setDescription("Lorem ipsum dolor est");
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			String jsonInString = mapper.writeValueAsString(task);
 
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      String jsonInString = mapper.writeValueAsString(task);
-
-      Map<String, String> headers = new HashMap<String, String>();
-      headers.put("Content-Type", "application/json");
-      return new APIGatewayProxyResponseEvent().withStatusCode(200).withHeaders(headers).withBody(jsonInString);
-    } catch(JsonProcessingException e) {
-      e.printStackTrace();
-      return new APIGatewayProxyResponseEvent().withStatusCode(500);
-    }
-  }
+			Map<String, String> headers = new HashMap<String, String>();
+			headers.put("Content-Type", "application/json");
+			return new APIGatewayProxyResponseEvent().withStatusCode(200).withHeaders(headers).withBody(jsonInString);
+		} catch(JsonProcessingException e) {
+			e.printStackTrace();
+			return new APIGatewayProxyResponseEvent().withStatusCode(500);
+		}
+	}
 
 }
--- src/main/java/com/carpinuslabs/todo/model/Task.java
+++ src/main/java/com/carpinuslabs/todo/model/Task.java
@@ -1,40 +1,49 @@
 package com.carpinuslabs.todo.model;
 
 public class Task {
-  private String title;
-  private String description;
-  private boolean done;
+	private String title;
+	private String description;
+	private boolean done;
+	private String test;
 
-  public Task() {
-    super();
-    this.done = false;
-  }
+	public Task() {
+		super();
+		this.done = false;
+	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-  public String getTitle() {
-    return this.title;
-  }
+	public String getTitle() {
+		return this.title;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-  public String getDescription() {
-    return this.description;
-  }
+	public String getDescription() {
+		return this.description;
+	}
 
-  public void setDone() {
-    this.done = true;
-  }
+	public void setDone() {
+		this.done = true;
+	}
 
-  public void setOpen() {
-    this.done = false;
-  }
+	public void setOpen() {
+		this.done = false;
+	}
 
-  public boolean isDone() {
-    return this.done;
-  }
+	public boolean isDone() {
+		return this.done;
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
 }
